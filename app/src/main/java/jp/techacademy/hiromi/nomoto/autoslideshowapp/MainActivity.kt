9f9c76a594a66cec9e2a5bd298af87216a018c07
hnmt2022 <chou0413bird@yahoo.co.jp>
2022-11-23 01:30:44 +0900

    パーミッション修正 ループ処理修正

--- app/src/main/java/jp/techacademy/hiromi/nomoto/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/hiromi/nomoto/autoslideshowapp/MainActivity.kt
@@ -12,13 +12,31 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
-class MainActivity<boolean> : AppCompatActivity() {
+class MainActivity: AppCompatActivity() {
 
     //private var mTimerSec = 0.0
     private var mTimer: Timer? = null
     private var mTimerRunning = false
 
     private val PERMISSIONS_REQUEST_CODE = 100
+
+
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            PERMISSIONS_REQUEST_CODE ->
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getContentsInfo()
+
+                }
+        }
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,24 +52,12 @@ class MainActivity<boolean> : AppCompatActivity() {
                     arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
                     PERMISSIONS_REQUEST_CODE
                 )
-                getContentsInfo()
+
             }
         }
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        when (requestCode) {
-            PERMISSIONS_REQUEST_CODE ->
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    getContentsInfo()
 
-                }
-        }
-        }
     private fun getContentsInfo() {
         // 画像の情報を取得する
         val resolver = contentResolver
@@ -71,7 +77,8 @@ class MainActivity<boolean> : AppCompatActivity() {
             if (cursor!!.moveToNext()) {
 
                 // indexからIDを取得し、そのIDから画像のURIを取得する
-                val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)//cursor.getColumnIndex()で現在cursorが指しているデータの中から画像のIDがセットされている位置を取得
+                val fieldIndex =
+                    cursor.getColumnIndex(MediaStore.Images.Media._ID)//cursor.getColumnIndex()で現在cursorが指しているデータの中から画像のIDがセットされている位置を取得
                 val id = cursor.getLong(fieldIndex)//cursor.getLong()で画像のIDを取得,カーソルクラスのgetLongメソッド
                 val imageUri = ContentUris.withAppendedId(
                     MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
@@ -82,6 +89,24 @@ class MainActivity<boolean> : AppCompatActivity() {
                 imageView.setImageURI(imageUri)
 
             }
+            if (!cursor.moveToNext()) {
+                cursor.moveToFirst()
+
+                val fieldIndex =
+                    cursor.getColumnIndex(MediaStore.Images.Media._ID)//cursor.getColumnIndex()で現在cursorが指しているデータの中から画像のIDがセットされている位置を取得
+                val id = cursor.getLong(fieldIndex)//cursor.getLong()で画像のIDを取得,カーソルクラスのgetLongメソッド
+                val imageUri = ContentUris.withAppendedId(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    id
+                )//ContentUris.withAppendedId()でそこから実際の画像のURIを取得してログに出力
+                // imageViewのsetImageURIメソッドを使い、URIが指している画像ファイルをImageViewに表示させていますmedia/13235
+                Log.d("ANDROID", "URI : " + imageUri.toString())
+                imageView.setImageURI(imageUri)
+
+
+
+            }
+
         }
         back_button.setOnClickListener {
             if (cursor!!.moveToPrevious()) {
@@ -93,6 +118,24 @@ class MainActivity<boolean> : AppCompatActivity() {
 
                 imageView.setImageURI(imageUri)
             }
+            if (!cursor.moveToPrevious()) {
+                cursor.moveToLast()
+
+                val fieldIndex =
+                    cursor.getColumnIndex(MediaStore.Images.Media._ID)//cursor.getColumnIndex()で現在cursorが指しているデータの中から画像のIDがセットされている位置を取得
+                val id = cursor.getLong(fieldIndex)//cursor.getLong()で画像のIDを取得,カーソルクラスのgetLongメソッド
+                val imageUri = ContentUris.withAppendedId(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    id
+                )//ContentUris.withAppendedId()でそこから実際の画像のURIを取得してログに出力
+                // imageViewのsetImageURIメソッドを使い、URIが指している画像ファイルをImageViewに表示させていますmedia/13235
+                Log.d("ANDROID", "URI : " + imageUri.toString())
+                imageView.setImageURI(imageUri)
+
+
+
+            }
+
         }
 
         start_reset_button.setOnClickListener {
@@ -109,50 +152,72 @@ class MainActivity<boolean> : AppCompatActivity() {
                 mTimer!!.cancel()
 
 
-                 mTimer = null//これを入れると止まらなくなる、が、入れていないと一度止めるとうごかなくなる→下にelseが無かったので加えたら正しく動いた
+                mTimer = null//これを入れると止まらなくなる、が、入れていないと一度止めるとうごかなくなる→下にelseが無かったので加えたら正しく動いた
                 //cursor.close()
             }
-                //!!はLesson3-12で学習したnot-nullアサーション演算子です。mTimerはnullでないことが明らかなので、強制的に非null型として処理を行っています
-                //タイマーの始動  タイマー始動の関数 schedule(TimerTask task, long delay, long period)
-                //} else
+            //!!はLesson3-12で学習したnot-nullアサーション演算子です。mTimerはnullでないことが明らかなので、強制的に非null型として処理を行っています
+            //タイマーの始動  タイマー始動の関数 schedule(TimerTask task, long delay, long period)
+            //} else
             else if (mTimer == null) {
                 start_reset_button.text = "停止"//ここら辺をrunの中に入れていたせいでクラッシュした
 
                 forward_button.isEnabled = false
                 back_button.isEnabled = false
 
-                    mTimer = Timer()
-                    mTimer!!.schedule(object : TimerTask() {
-                        // TimerTask task にはタイマー処理を記述するためのクラスを作成してインスタンスを渡します。
-                        //mTimer.schedule() を使うと、アプリが終了するまで、run() 内のコードを実行し続けます。
-                        override fun run() { //TimerTask の中の run() 関数が指定時間毎に呼び出される仕組み
+                mTimer = Timer()
+                mTimer!!.schedule(object : TimerTask() {
+                    // TimerTask task にはタイマー処理を記述するためのクラスを作成してインスタンスを渡します。
+                    //mTimer.schedule() を使うと、アプリが終了するまで、run() 内のコードを実行し続けます。
+                    override fun run() { //TimerTask の中の run() 関数が指定時間毎に呼び出される仕組み
 
 
+                        mHandler.post {
 
-                            mHandler.post {
+                            if (cursor!!.moveToNext()) {
+                                //  do{
+                                val fieldIndex =
+                                    cursor.getColumnIndex(MediaStore.Images.Media._ID)
+                                val id = cursor.getLong(fieldIndex)
+                                val imageUri = ContentUris.withAppendedId(
+                                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                                    id
+                                )
 
-                                if (cursor!!.moveToNext()) {
-                                    //  do{
-                                    val fieldIndex =
-                                        cursor.getColumnIndex(MediaStore.Images.Media._ID)
-                                    val id = cursor.getLong(fieldIndex)
-                                    val imageUri = ContentUris.withAppendedId(
-                                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                                        id
-                                    )
+                                imageView.setImageURI(imageUri)
 
-                                    imageView.setImageURI(imageUri)
+                                // cursor.close()
 
-                                    // cursor.close()
-                                }
                             }
-                            //mTimerSec += 2.0
+
+                            if (!cursor.moveToNext()) {
+                                cursor.moveToFirst()
+
+
+                                val fieldIndex =
+                                    cursor.getColumnIndex(MediaStore.Images.Media._ID)//cursor.getColumnIndex()で現在cursorが指しているデータの中から画像のIDがセットされている位置を取得
+                                val id =
+                                    cursor.getLong(fieldIndex)//cursor.getLong()で画像のIDを取得,カーソルクラスのgetLongメソッド
+                                val imageUri = ContentUris.withAppendedId(
+                                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                                    id
+                                )//ContentUris.withAppendedId()でそこから実際の画像のURIを取得してログに出力
+                                // imageViewのsetImageURIメソッドを使い、URIが指している画像ファイルをImageViewに表示させていますmedia/13235
+                                Log.d("ANDROID", "URI : " + imageUri.toString())
+                                imageView.setImageURI(imageUri)
+
+                            }
                         }
+                        //mTimerSec += 2.0
+                    }
 
-                    }, 2000, 2000)
+
+                }, 2000, 2000)
 
 
-                }
             }
+
         }
+    }
 }
+
+
